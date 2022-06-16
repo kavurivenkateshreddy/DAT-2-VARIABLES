dde8819f31f7fde09f0cda8c093468f41bc8021d
Venkatesh Reddy Kavuri <104256379+kavurivenkateshreddy@users.noreply.github.com>
2022-06-16 19:49:08 +0530

    Update variables.py

--- variables.py
+++ variables.py
@@ -7,7 +7,32 @@ File Desc: variable rules,way to assign variables,isidentifier()
    numerical >         middle : end
  under scrol > start : middle ; end
  we should not enter any space in between the variable 
- we can reuse the variables there is no limites for the naming of variables.'''
+ we can reuse the variables there is no limites for the naming of variables.
+ 
+RULES OF VARIABLES:
+-------------------------
+1) A variable is mostly on left side.
+
+2) Variables are used for storing the values.
+
+3) Variables works like containers.
+
+4) A variable should start with alphabet
+   it can be lower case or upper case.
+
+5) Python is case-sensitive.
+   There is a d/b lower case and upper case letters.
+
+6) Variables cannot contain keywords.
+
+7) A Variable can't start with numeric.
+
+8) A variable can start with underscore _ Symbol
+
+9) Except underscore other symbols are not allowed.
+
+10) A variable should not a space between them.
+ '''
 a=1;b=2
 print(a) #1
 print(b) #2
@@ -29,6 +54,9 @@ print(h) #6
 print(f,g,h) #6 6 6
 print(f,i,j) #6 7 8
 # print(k) #Name Error: name 'k' is not defined
+""" ISIDENTIFIER
+---> isidentifier is used to find the variables are valied or invalied
+---> is > it is in the form of bool(true or false)"""
 print('a'.isidentifier()) #True
 print('_'.isidentifier()) #True
 print('_a'.isidentifier()) #True
